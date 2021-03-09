7b0f78bac762f7166328e4ea99183a05b4ac5e0d
Kwondohyung <kdh906@naver.com>
2021-03-09 15:39:35 +0900

    static사용하여 부트스트랩적용, html template상속

--- fcdjango/settings.py
+++ fcdjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'board',
-    'fcuser'
+    'fcuser',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
---
+++ readme.txt
@@ -0,0 +1,11 @@
+*** 앱 등록시, 모델이 변경시 사용 명령어
+python manage.py makemigrations
+python manage.py migrate
+
+*** 서버실행
+python manage.py runserver
+
+
+*** 부트스트랩사용
+1. cdn방식
+2. static방식, bootswatch에서 테마다운로드 (https://bootswatch.com/)
