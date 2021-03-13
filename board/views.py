75eb90e156e56fa43b3d2955fefb713b9a7d390e
Kwondohyung <kdh906@naver.com>
2021-03-13 16:20:43 +0900

    BOARD, Tag 모델객체 구현 및 Board 구현

--- board/views.py
+++ board/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import redirect, render
+from django.core.paginator import Paginator
+from board.models import Board
+from tag.models import Tag
+from board.forms import BoardForm
+from fcuser.models import Fcuser
 
 # Create your views here.
+
+
+def board_detail(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다')
+
+    return render(request, 'board_detail.html', {'board': board})
+
+
+def board_write(request):
+    if not request.session.get('user'):
+        return redirect('/fcuser/login/')
+
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            fcuser = Fcuser.objects.get(pk=user_id)
+
+            tags = form.cleaned_data['tags'].split(',')
+
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = fcuser
+            board.save()
+
+            for tag in tags:
+                if not tag:
+                    continue
+
+                _tag, _ = Tag.objects.get_or_create(name=tag)
+                board.tags.add(_tag)
+
+            return redirect('/board/list/')
+    else:
+        form = BoardForm()
+
+    return render(request, 'board_write.html', {'form': form})
+
+
+def board_list(request):
+    all_boards = Board.objects.all().order_by('-id')
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(all_boards, 3)
+
+    boards = paginator.get_page(page)
+    return render(request, 'board_list.html', {'boards': boards})
